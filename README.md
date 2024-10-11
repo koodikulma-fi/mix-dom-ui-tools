b83a749eac2b64efdebe74812ab7e4626dcdb85d
Mikael Sauren <bertel3@gmail.com>
2024-10-12 00:28:19 +0300

    Adds initial README.

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## What
+
+- This module contains simple components for common UI use cases for MixDOM library.
+- The package contains sub modules - only import specifically what you need (to keep the bundle size small).
+
+## Todo
+
+- Very early state. Not yet bundled. And components not refined. Copied the components from mixdomjs.org source code.
--- dist/modules/index.js
+++ dist/modules/index.js
@@ -1,4 +1,9 @@
 // - JSX - //
+//
+// <- -BTW.... DO WE FORCE JSX_camelCase ... ? .. WE'D LIKE TO JUST BE TRANSPARENT..! WE DON'T CARE FOR EXPORTS..!
+// ... I think can work..... we just must exclude the global declaration from bundled code...  .. hopefully will work that way..
+// ...... If doesn't.. Then just in typing and package json.... Try to somehow solve..?
+// ........  --> Different package declarations, that actually use same folders..! And just the .d.ts is differnt..!! Yes..!
 // - Export all - //
 // No internal dependencies.
 export * from "./spreads";
